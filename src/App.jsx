85afe857c33c62d08ebb8a986857e3cbe9e3a462
AndrewHo <48797504+AndrewHo1222@users.noreply.github.com>
2024-01-10 07:28:48 +0000

    Modify info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Andrew Ho",
+  title: "Student Developer",
+  email: "hoandrew1222@gmail.com",
+  gitHub: "Andrewho1222",
+  instagram: "",
+  linkedIn: "andrewhohsh",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
